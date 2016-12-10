2f81a54280749a2767bc4572f3bfa58c6cebec71
CharcoStudios <charcostudios@gmail.com>
2016-12-11 00:58:28 +0100

    Monogame port

---
+++ monocpc/BinaryFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace monocpc {
+    public class BinaryFile {
+        public Byte[] m_data;
+        public int m_data_size;
+    }
+}
